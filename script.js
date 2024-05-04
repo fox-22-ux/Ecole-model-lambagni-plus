1301f426ed9bb6f8be34059f2f0294ba32319062
fox-22-ux <117744338+fox-22-ux@users.noreply.github.com>
2024-05-04 20:17:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navConsole = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navConsole) {
+    navConsole.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== SHADOW HEADER ===============*/
+
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ?  header.classList.add('shadow-header')
+                        : header.classList.remove('shadow-header')
+}
+
+window.addEventListener('scroll', shadowHeader)
+
+
+/*=============== EMAIL JS ===============*/
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact__message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('', '', '', '')
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add("show-scroll")
+                                        : scrollUp.classList.remove("show-scroll")
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop+ sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>  themButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-img, .services-container, .class-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home__perfil, .about__image, .conctact__mail', { origin: 'right' });
+ScrollReveal().reveal(`.home__name, .home__info, .about__container, section__title-1,
+.about__info, .contact__social, .contact__data`, { origin: 'left' });
+
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Ecole Moderne', "Lanbagni", "Plus", "Bienvenue!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+
